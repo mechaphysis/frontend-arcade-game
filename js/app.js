7eae91939552723579207de4c7d67e777a5eff30
mechaphysis <triptofante@gmail.com>
2018-07-19 17:53:26 +0200

    feat: Add default values for Enemy position/speed and player location

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    //Setting initial enemy location and speed:
+    this.location = x;
+    this.speed = speed ;
 };
 
 // Update the enemy's position, required method for game
@@ -24,8 +27,10 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-let Player = function() {
+let Player = function(x,y) {
   this.sprite = 'images/char-pink-girl.png';
+  this.x = x;
+  this.y = y;
 };
 
 Player.prototype.update = function(dt) {
@@ -43,7 +48,6 @@ Player.prototype.handleInput = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 
 
 // This listens for key presses and sends the keys to your
